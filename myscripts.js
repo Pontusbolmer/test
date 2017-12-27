246c7b6f995ee90af19274149ace6af9c9ce9b92
Pontusbolmer <ponteeee@live.se>
2017-12-27 14:50:27 +0000

    Changes

--- myscripts.js
+++ myscripts.js
@@ -17,10 +17,42 @@ $( document ).ready(function() {
         function createUIFromLoadedProducts() {
             for (var i = 0; i < listOfProducts.length; i++) {
                   var card = listOfProducts[i].countryname;
-                  $(".hmeanu").append('<a href="#">'+card+ " " +'</a>');
+                  $(".hmeanu").append('<a href="#" class="menuItem"> '+card+ " " +'</a>');
             }
       }
+
+      $(".productBox").hide();
+
+      /*$( ".hmeanu" ).hover(function() {
+        $( ".productBox" ).toggle();
+      });
+      */
+
+      var bajsskalle;
+
+      fetch("underkategorier.json")
+      .then(function(response) {
+          return response.json();
+      })
+      .then(function(bajs) {
+          bajsskalle = bajs;
+          createUIFromLoadedbajs();
+          console.log(bajsskalle);  
+  
+        
+      });
+
+      function createUIFromLoadedbajs() {
+          for (var i = 0; i < bajsskalle.length; i++) {
+                var productBox = bajsskalle[i].stadname;
+                $(".productBox").append('<a href="#" class="menuItem"> '+productBox+ " " +'</a>');
+
+           
+        }
+    }
  
+
+
 
 
 });
